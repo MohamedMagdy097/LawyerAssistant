9518d59dde48f4c0284f3979235f6e31155f7bf3
Mohamed Magdy <zalatanback5@gmail.com>
2023-12-16 00:38:01 +0200

    +db

--- app.js
+++ app.js
@@ -1,13 +1,154 @@
-import OpenAI from "openai";
 import { config } from "dotenv";
 config();
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
 import fs from "fs";
 import express from 'express';
-const app = express();
+import postgres from "postgres";
 import cors from "cors";
-const openai = new OpenAI();
+import OpenAI from "openai";
 
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
+
+
+// DATABASE FUNCTIONS
+// Get database version
+async function getPgVersion() {
+  const result = await sql`select version()`;
+  console.log("Database Connected\n" , result[0].version);
+}
+
+// Fetch all accounts
+async function allLawyers() {
+  const result = await sql`select * from lawyer`;
+  console.log(result);
+};
+
+// Fetch all todos
+async function allTodos() {
+  const result = await sql`select * from todos`;
+  console.log(result);
+};
+
+// fetch all todos assigned for or by users and its status
+async function allTodosStatus() {
+  const result = await sql`select * from donetodo`;
+  console.log(result);
+}
+
+// createAccount(name, password, jobtitle, type, email, sup_id)
+async function createAccount(name, password, job_title, type, email, sup_id) {
+  const result = await sql`INSERT INTO lawyer (name, password, job_title, type, email, sup_id)
+  VALUES
+     (${name}, ${password}, ${job_title}, ${type}, ${email}, ${sup_id});`;
+  console.log(result);
+  return result;
+}
+
+// check if account exists with email and password
+async function login(email, password) {
+  const result = await sql`
+    SELECT *
+    FROM lawyer
+    WHERE email = ${email} AND password = ${password}
+  `;
+  console.log(result);
+
+  // Check if a lawyer exists based on the query result
+  if (result.length > 0) {
+    console.log('Lawyer exists!');
+  } else {
+    console.log('Lawyer does not exist.');
+  }
+  return result;
+}
+
+// check if account exists with email
+async function checkEmail(email) {
+  const result = await sql`
+    SELECT *
+    FROM lawyer
+    WHERE email = ${email}
+  `;
+  console.log(result);
+
+  // Check if an email exists based on the query result
+  if (result.length > 0) {
+    console.log('email exists!');
+  } else {
+    console.log('email does not exist.');
+  }
+  return result;
+}
+
+// fetch all todos for a supervisor
+async function fetchSupTodos(lawyer_id) {
+  const result = await sql`
+    SELECT *
+    FROM todos
+    WHERE l_id = ${lawyer_id}
+  `;
+  console.log(result);
+  return result;
+}
+
+// fetch all todos for a lawyer
+async function fetchTodoIds(lawyer_id) {
+  const result = await sql`
+    SELECT todo_id
+    FROM donetodo
+    WHERE l_id = ${lawyer_id}
+  `;
+  console.log(result);
+  return result;
+}
+
+// create a todo
+async function createTodo(title, description, deadline, l_id) {
+  const result = await sql`
+    INSERT INTO todos (title, description, deadline, l_id)
+    VALUES
+      (${title}, ${description}, ${deadline}, ${l_id})
+  `;
+  console.log(result);
+  return result;
+}
+
+// fetch a todo id
+async function fetchTodoId(title, description, deadline, l_id) {
+  const result = await sql`
+    SELECT id
+    FROM todos
+    WHERE title = ${title} AND description = ${description} AND deadline = ${deadline} AND l_id = ${l_id}
+  `;
+  console.log(result[0]);
+  return result[0].id;
+}
+
+// asssign a todo to lawyers
+async function assignTodoToLawyers(todo_id, l_id) {
+  const result = await sql`
+    INSERT INTO donetodo (l_id, todo_id, done)
+    VALUES
+      (${l_id}, ${todo_id}, false)
+  `;
+  console.log(result);
+}
+
+// delete a todo
+
+const openai = new OpenAI();
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -160,7 +301,6 @@ async function deleteThread(threadId) {
 
   console.log(response);
 }
-
 
 //
 // MESSAGES
@@ -327,6 +467,29 @@ async function listSteps(threadId, runId) {
 // uploadFile('filename.txt');
 // fileToAssistant('file-i7ZXjwbamadWyc2qTtVUaeLa', assistantId);
 
+var currentThreadId = "";
+var currentRunId = "";
+var currentRunStatus = "";
+var currentMsgId = "";
+var currentResponse = "";
+var lastOutputId = "";
+var lawyer;
+var todos;
+var todoIds;
+
+// removing unncessary string from date
+function cleanDate(date1) {
+  let dateFromDatabase = new Date(date1);
+  // Convert to string with the desired format
+
+  let formattedDateString = dateFromDatabase.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
+  return formattedDateString;
+}
+
 // Get Last Output ID
 async function getLastId(threadId) {
   const threadMessages = await openai.beta.threads.messages.list(
@@ -336,14 +499,6 @@ async function getLastId(threadId) {
   return threadMessages.body.first_id;
 }
 
-var currentThreadId = "";
-var currentRunId = "";
-var currentRunStatus = "";
-var currentMsgId = "";
-var currentResponse = "";
-var lastOutputId = "";
-
-
 // Create new thread / Conversation
 app.get('/new', async (req, res) => {
   currentThreadId = await createThread();
@@ -351,6 +506,7 @@ app.get('/new', async (req, res) => {
   res.send(currentThreadId);
 });
 
+// Test
 app.post('/test', (req, res) => {
   let threadId = "";
   let msgg = "";
@@ -364,7 +520,7 @@ app.post('/test', (req, res) => {
   });
 });
 
-
+// Chat with the assistant
 app.post('/chat', async (req, res) => {
   var msg = req.body.msg;
   var threadId = req.body.threadId;
@@ -391,10 +547,191 @@ app.post('/chat', async (req, res) => {
   res.end();
 });
 
+// Delete a thread
+app.post('/delete', async (req, res) => {
+  let threadId = req.body.threadId;
+
+  await deleteThread(threadId);
+
+  res.send("Thread deleted");
+});
+
+
 app.get('/', (req, res) => {
   res.send("Hello World");
+});
+
+
+// register a new account
+app.post('/register', async (req, res) => {
+  let name = req.body.name;
+  let password = req.body.password;
+  let job_title = req.body.job_title;
+  let type = req.body.type;
+  let email = req.body.email;
+  let sup_id = req.body.sup_id;
+
+  let emailExists = checkEmail(email);
+  if (emailExists.length > 0) {
+    res.send("Email already exists");
+  }
+
+  let result = await createAccount(name, password, job_title, type, email, sup_id);
+
+  if(result.length == 0)
+    res.send("Account created");
+  else
+    res.send("Account creation failed");
+});
+
+// login to an account
+app.post('/login', async (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  let emailExists = await checkEmail(email);
+  
+  if (emailExists.length > 0) {
+    let result = await login(email, password);
+
+    if(result.length == 0) {
+      res.send("wrong password");
+    }
+
+    else {
+      res.write(JSON.stringify(result[0]));
+      lawyer = result[0];
+
+      if (lawyer.type == "supervisor") {
+        // Fetching todos for supervisor
+        todos = await fetchSupTodos(lawyer.id);
+
+        if (todos.length > 0) {
+          // Removing unnecessary string from date
+          for (let i = 0; i < todos.length; i++) {
+            todos[i].deadline = cleanDate(todos[i].deadline);
+          }
+          res.write(JSON.stringify(todos));
+        }
+        else {
+          res.write("No todos");
+        }
+      }
+      else {
+        // Fetching todos for lawyer
+        todoIds = await fetchTodoIds(lawyer.id);
+
+        if (todoIds.length > 0) {
+          for(let i = 0; i < todoIds.length; i++) {
+            todos[i] = await sql`
+              SELECT *
+              FROM todos
+              WHERE id = ${todoIds[i].todo_id}
+            `;
+          }
+
+          // Removing unnecessary string from date
+          for (let i = 0; i < todos.length; i++) {
+            todos[i].deadline = cleanDate(todos[i].deadline);
+          }
+          res.write(JSON.stringify(todos));
+        }
+        else {
+          res.write("No todos");
+        }
+      }
+      
+      res.end();
+    }
+  }
+
+  else {
+    res.send("Email does not exist");
+  }
+
+});
+
+// create a todo
+app.post('/create', async (req, res) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  let deadline = req.body.deadline;
+  let l_id = req.body.l_id;
+  // in production
+  // let l_id = lawyer.id;
+  let junior_id = parseInt(req.body.junior_ids);
+
+  let result = await createTodo(title, description, deadline, l_id);
+
+  if(result.length == 0) {
+    res.write("Todo created");
+
+    let todo_id = await fetchTodoId(title, description, deadline, l_id);
+
+    await assignTodoToLawyers(todo_id, junior_id);
+    res.end();
+  }
+  else {
+    res.send("Todo creation failed");
+  }
+});
+
+// delete a todo
+app.post('/deletetodo', async (req, res) => {
+  let id = req.body.id;
+
+  let result = await sql`
+    DELETE FROM todos
+    WHERE id = ${id}
+  `;
+
+  if(result.length == 0) {
+    res.send("Todo deleted");
+  }
+  else {
+    res.send("Todo deletion failed");
+  }
+});
+
+// search for a user name
+app.post('/namesearch', async (req, res) => {
+
+  let name = req.body.name;
+
+  let result = await sql`
+    SELECT *
+    FROM lawyer
+    WHERE name LIKE '%' || ${name} || '%';
+  `;
+
+  if (result.length > 0) {
+    res.json(result);
+  } else {
+    res.send("User not found");
+  }
+
+});
+
+// search for a user email
+app.post('/emailsearch', async (req, res) => {
+
+  let email = req.body.email;
+
+  let result = await sql`
+    SELECT *
+    FROM lawyer
+    WHERE email LIKE '%' || ${email} || '%';
+  `;
+
+  if (result.length > 0) {
+    res.json(result);
+  } else {
+    res.send("User not found");
+  }
+
 });
 
 app.listen(4000, () => {
   console.log('Server connected');
 });
+await getPgVersion();
